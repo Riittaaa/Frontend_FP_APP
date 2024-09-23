01a44c5a8221617b9bb83d0d23c2ccda1dc1a84e
Rita Gainju <gainju.rita123@gmail.com>
2024-09-24 01:12:07 +0545

    refactor: hiding the nav-items if not logged in and in the login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logout } from "../store/authSlice";
 import logo from "../images/logo.png";
 import "../css/Navbar.css";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+
+  useEffect(() => {
+    setIsMenuOpen(false);
+    setIsLoggedIn(!!localStorage.getItem("token"));
+  }, [location]);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -12,6 +23,12 @@ function Navbar() {
 
   const closeMenu = () => {
     setIsMenuOpen(false);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    dispatch(logout());
+    setIsLoggedIn(false);
   };
 
   return (
@@ -29,41 +46,50 @@ function Navbar() {
           isMenuOpen ? "navbar__nav-links--active" : ""
         }`}
       >
-        <li>
-          <Link to="/dashboard" onClick={closeMenu}>
-            <i className="fas fa-tachometer-alt"></i> Dashboard
-          </Link>
-        </li>
-        <li>
-          <Link to="/customers" onClick={closeMenu}>
-            <i className="fas fa-users"></i> Customers
-          </Link>
-        </li>
-        <li>
-          <Link to="/goods" onClick={closeMenu}>
-            <i className="fas fa-box-open"></i> Goods
-          </Link>
-        </li>
-        <li>
-          <Link to="/vehicles" onClick={closeMenu}>
-            <i className="fas fa-truck"></i> Vehicles
-          </Link>
-        </li>
-        <li>
-          <Link to="/orders" onClick={closeMenu}>
-            <i className="fas fa-shopping-cart"></i> Orders
-          </Link>
-        </li>
-        <li>
-          <Link to="/drivers" onClick={closeMenu}>
-            <i className="fas fa-tags"></i> Drivers
-          </Link>
-        </li>
-        <li className="navbar__login-item">
-          <Link to="/login" className="navbar__login" onClick={closeMenu}>
-            <i className="fas fa-user"></i> Login
-          </Link>
-        </li>
+        {isLoggedIn ? (
+          <>
+            <li>
+              <Link to="/dashboard" onClick={closeMenu}>
+                <i className="fas fa-tachometer-alt"></i> Dashboard
+              </Link>
+            </li>
+            <li>
+              <Link to="/customers" onClick={closeMenu}>
+                <i className="fas fa-users"></i> Customers
+              </Link>
+            </li>
+            <li>
+              <Link to="/goods" onClick={closeMenu}>
+                <i className="fas fa-box-open"></i> Goods
+              </Link>
+            </li>
+            <li>
+              <Link to="/vehicles" onClick={closeMenu}>
+                <i className="fas fa-truck"></i> Vehicles
+              </Link>
+            </li>
+            <li>
+              <Link to="/drivers" onClick={closeMenu}>
+                <i className="fas fa-tags"></i> Drivers
+              </Link>
+            </li>
+            <li className="navbar__logout-item">
+              <Link
+                to="/login"
+                className="navbar__login"
+                onClick={handleLogout}
+              >
+                <i className="fas fa-sign-out-alt"></i> Logout
+              </Link>
+            </li>
+          </>
+        ) : (
+          <li className="navbar__login-item">
+            <Link to="/login" className="navbar__login" onClick={closeMenu}>
+              <i className="fas fa-user"></i> Login
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
