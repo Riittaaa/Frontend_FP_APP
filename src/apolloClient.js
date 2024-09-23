f6cfaa05d87155728d2d5e243cef9d8f54418c43
Rita Gainju <gainju.rita123@gmail.com>
2024-09-24 01:06:59 +0545

    fix: fixed header for authorization

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -2,14 +2,17 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3000/graphql",
+  // uri: "http://localhost:3000/graphql",
+  uri: "https://aabd-2400-1a00-b060-3c77-662a-d377-8642-78d9.ngrok-free.app/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("token");
   return {
-    ...headers,
-    authorization: token ? `Bearer ${token}` : "",
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
   };
 });
 
