99bd3dcdded6b64320c3f986bfd68e549f24b310
Rita Gainju <gainju.rita123@gmail.com>
2024-10-02 14:23:39 +0545

    feat: added reactivated button if soft deleted vehicle

--- src/components/Table/ActionCellRenderer.js
+++ src/components/Table/ActionCellRenderer.js
@@ -1,22 +1,33 @@
 import React from "react";
 import "../../css/ActionCellRenderer.css";
 
-const ActionCellRenderer = ({ data, onEdit, onDelete }) => {
+const ActionCellRenderer = ({ data, onEdit, onDelete, onReactivate }) => {
   return (
     <div className="actions">
-      <button
-        className="action-button action-button--edit"
-        onClick={() => onEdit(data.id)}
-      >
-        Edit
-      </button>
+      {data.deletedAt ? (
+        <button
+          className="action-button action-button--reactivate"
+          onClick={() => onReactivate(data.id)}
+        >
+          Reactivate
+        </button>
+      ) : (
+        <>
+          <button
+            className="action-button action-button--edit"
+            onClick={() => onEdit(data.id)}
+          >
+            Edit
+          </button>
 
-      <button
-        className="action-button action-button--delete"
-        onClick={() => onDelete(data.id)}
-      >
-        Delete
-      </button>
+          <button
+            className="action-button action-button--delete"
+            onClick={() => onDelete(data.id)}
+          >
+            Delete
+          </button>
+        </>
+      )}
     </div>
   );
 };
